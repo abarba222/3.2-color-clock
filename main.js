345b618b632ba2909ebfd62765adec22ed52df42
Anthony Barba <abarba222@gmail.com>
2015-05-19 16:36:19 -0400

    achieved functioning clock and centered time.

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-var $hours;
-var $minutes;
-var $seconds;
+ var $hours;
+ var $minutes;
+ var $seconds;
 
-var currentTime = new Date();
+function displayCurrentTime() {
 
-function clock(){
-currentTime = new Date();
+  var currentTime = new Date();
+
+  var currentHour = currentTime.getHours();
+  console.log(currentHour);
+  $hours = document.querySelector('.hours');
+  $hours.textContent = currentHour + ":";
+
+  var currentMinute = currentTime.getMinutes();
+  console.log(currentMinute);
+  $minutes = document.querySelector('.minutes');
+  $minutes.textContent = currentMinute + ":";
+
+  var currentSecond = currentTime.getSeconds();
+  console.log(currentSecond);
+  $seconds = document.querySelector('.seconds');
+  $seconds.textContent = currentSecond;
+
 }
 
-var currentHour = currentTime.getHours();
-console.log(currentHour);
-$hours = document.querySelector('.hours');
-$hours.textContent = currentHour + ':';
+displayCurrentTime();
 
-var currentMinute = currentTime.getMinutes();
-console.log(currentMinute);
-$minutes = document.querySelector('.minutes');
-$minutes.textContent = currentMinute + ':';
-
-var currentSecond = currentTime.getSeconds();
-console.log(currentSecond);
-$seconds = document.querySelector('.seconds');
-$seconds.textContent = currentSecond;
-
-
-function timeUpdate(event){
-
-var intervalID = window.setInterval(clock, 1000);
-}
+var intervalID = window.setInterval(displayCurrentTime, 1000);
